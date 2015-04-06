6dbec17e899f663b0c981d17629b2401064cae65
Doug Scandrett <doug@dssdigital.com>
2015-04-06 16:18:04 -0400

    Add in a designable root class for convenience.

---
+++ BladeKit/src/DesignableView.swift
@@ -0,0 +1,55 @@
+//
+//  DesignableView.swift
+//  BladeKit
+//
+//  Original by Kristin
+//  Migrated to BladeKit by Doug on 4/6/15.
+//  Copyright (c) 2015 Blade. All rights reserved.
+//
+
+import UIKit
+
+class DesignableView: UIView {
+    
+    var view: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadXib()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadXib()
+    }
+    
+    private func loadXib() {
+        
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: xibName(), bundle: bundle)
+        
+        // Assumes UIView is top level and only object in .xib file
+        view = nib.instantiateWithOwner(self, options: nil)[0] as UIView
+        
+        // use bounds not frame or it'll be offset
+        view.frame = bounds
+        
+        // Make the view stretch with containing view
+        view.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        
+        // Do any custom setup
+        xibSetup()
+        
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(view)
+    }
+    
+    // This must be overridden in subclass
+    func xibName() -> String {
+        fatalError("xibName() has not been implemented")
+    }
+    
+    // Override if you need a hook for additional setup after .xib is loaded
+    func xibSetup() {
+    }
+}
