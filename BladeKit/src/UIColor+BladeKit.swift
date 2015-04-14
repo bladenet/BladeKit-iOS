ac21a3c79999f1dab76df206ba3fee5e78806b6c
Doug Scandrett <doug@dssdigital.com>
2015-04-14 16:43:47 -0400

    Migrate in a few more generic extensions.

---
+++ BladeKit/src/UIColor+BladeKit.swift
@@ -0,0 +1,31 @@
+//
+//  UIColor+BladeKit.swift
+//  BladeKit
+//
+//  Created by Doug on 4/13/15.
+//  Copyright (c) 2015 BladeKit, Inc. All rights reserved.
+//
+
+import Foundation
+
+public extension UIColor {
+    public static func colorFromInitials(initials: String) -> UIColor {
+        var total = 0.0
+        var index = 0.0
+        var utf8A: UnicodeScalar {
+            for code in "A".unicodeScalars {
+                return code
+            }
+            return UnicodeScalar(1) // useless
+        }
+        for codeUnit in initials.unicodeScalars {
+            let code = codeUnit.value - utf8A.value
+            let denom = Double(pow(26.0, index + 1.0))
+            let addition = Double(code) / denom
+            total += addition
+            index++
+        }
+        total = floor((total == 1.0) ? 255.0 : total * 256.0)
+        return UIColor(hue: CGFloat(total/255.0), saturation: 170.6752/255.0, brightness: 170.0/255.0, alpha: 1.0)
+    }
+}
