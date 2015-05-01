164341a0f8497eb71b14586326d9c56729ce3046
Brian Bates <bsbates@gmail.com>
2015-05-01 10:47:22 -0400

    Version Utilities - Added basic utilites for version strings
    - Format <major>.<minor>.<patch>
    - Functions to extract sections from a version string. Defaults to 0
    - Function to compare two version strings
    - And unit tests, of course

---
+++ BladeKit/src/VersionUtils.swift
@@ -0,0 +1,48 @@
+//
+//  VersionUtils.swift
+//  BladeKit
+//
+//  Created by Brian Bates on 5/1/15.
+//  Copyright (c) 2015 Blade. All rights reserved.
+//
+
+import Foundation
+
+public class VersionUtils {
+
+    private class func getVersionAtIndex(versionString: String, index: Int) -> Int {
+        var pieces = split(versionString) {$0 == "."}
+        if pieces.count > index, let version = pieces[index].toInt() {
+            return version
+        }  else {
+            return 0
+        }
+    }
+    
+    public class func majorVersion(versionString: String) -> Int {
+        return getVersionAtIndex(versionString, index: 0)
+    }
+    
+    public class func minorVersion(versionString: String) -> Int {
+        return getVersionAtIndex(versionString, index: 1)
+    }
+    
+    public class func patchVersion(versionString: String) -> Int {
+        return getVersionAtIndex(versionString, index: 2)
+    }
+    
+    // Comparator for version strings.
+    // Returns -1 if v1 < 2, 0 if v1 == v2 and 1 if v1 > v2
+    public class func compareVersions(v1: String, v2: String) -> Int {
+        if majorVersion(v1) != majorVersion(v2) {
+            return majorVersion(v1) - majorVersion(v2)
+        } else if minorVersion(v1) != minorVersion(v2) {
+            return minorVersion(v1) - minorVersion(v2)
+        } else if patchVersion(v1) != patchVersion(v2) {
+            return patchVersion(v1) - patchVersion(v2)
+        } else {
+            return 0
+        }
+    }
+
+}
---
+++ BladeKitTests/UtilitiesTests.swift
@@ -0,0 +1,62 @@
+//
+//  UtilitiesTests.swift
+//  BladeKit
+//
+//  Created by Brian Bates on 5/1/15.
+//  Copyright (c) 2015 Blade. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import BladeKit
+
+class UtilitiesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: Version Utilities Tests
+    
+    func testMajorVersion() {
+        XCTAssertEqual(1, VersionUtils.majorVersion("1.0.0"), "Major version fail")
+        XCTAssertEqual(0, VersionUtils.majorVersion(""), "Major version fail")
+        XCTAssertEqual(0, VersionUtils.majorVersion("foobar.1.0"), "Major version fail")
+        XCTAssertEqual(0, VersionUtils.majorVersion("0.1.1"), "Major version fail")
+        XCTAssertEqual(4, VersionUtils.majorVersion("4"), "Major version fail")
+        XCTAssertEqual(4, VersionUtils.majorVersion("4.0"), "Major version fail")
+    }
+    
+    func testMinorVersion() {
+        XCTAssertEqual(0, VersionUtils.minorVersion("1.0.0"), "Minor version fail")
+        XCTAssertEqual(0, VersionUtils.minorVersion("4"), "Minor version fail")
+        XCTAssertEqual(3, VersionUtils.minorVersion("4.3"), "Minor version fail")
+        XCTAssertEqual(0, VersionUtils.minorVersion(""), "Minor version fail")
+        XCTAssertEqual(0, VersionUtils.minorVersion("1.foobar.0"), "Minor version fail")
+        XCTAssertEqual(1, VersionUtils.minorVersion("0.1.1"), "Minor version fail")
+    }
+    
+    func testPatchVersion() {
+        XCTAssertEqual(0, VersionUtils.patchVersion("1.0.0"), "Patch version fail")
+        XCTAssertEqual(0, VersionUtils.patchVersion("4"), "Patch version fail")
+        XCTAssertEqual(12, VersionUtils.patchVersion("4.3.12"), "Patch version fail")
+        XCTAssertEqual(0, VersionUtils.patchVersion(""), "Patch version fail")
+        XCTAssertEqual(0, VersionUtils.patchVersion("1.1.foobar"), "Patch version fail")
+        XCTAssertEqual(1, VersionUtils.patchVersion("0.1.1"), "Patch version fail")
+    }
+    
+    func testCompareVersions() {
+        XCTAssert(VersionUtils.compareVersions("1.0.0", v2: "1.0.1") < 0, "Compare versions fail")
+        XCTAssert(VersionUtils.compareVersions("1.0.1", v2: "1.0.0") > 0, "Compare versions fail")
+        XCTAssert(VersionUtils.compareVersions("1.2.3", v2: "1.2.3") == 0, "Compare versions fail")
+        XCTAssert(VersionUtils.compareVersions("2.0.0", v2: "1.3.12") > 0, "Compare versions fail")
+        XCTAssert(VersionUtils.compareVersions("1.12.0", v2: "1.13.3") < 0, "Compare versions fail")
+        XCTAssert(VersionUtils.compareVersions("", v2: "1") < 0, "Compare versions fail")
+        XCTAssert(VersionUtils.compareVersions("1", v2: "1.0.0") == 0, "Compare versions fail")
+        XCTAssert(VersionUtils.compareVersions("1", v2: "1.0.1") < 0, "Compare versions fail")
+    }
+}
